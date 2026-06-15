3b7a41160e9a0d1e95d654eac9007e03c7266c0f
23489ac <23489ac@hvhs.school.nz>
2026-06-15 00:52:01 +0000

    Jun 15, 2026, 12:52 AM

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 console.log("Hello world!")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
  let item = ["Cupcakes", 'Burgar",  "Fries", "cheese Melt"];
-    
+function addItem() {
+    const itemField = document.getElementById("itemField");
+    let userItem = itemField.value;
+    shoppingList.push(userItem)
+    OUTPUT.innerHTML += "<p>You have added " + userItem + " to the list </p>";
+
+}
+function showList() {
+    OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>";
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += "<p>" + shoppingList[i] + "</p>";
+    }
+}
