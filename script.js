c263daa3fcddc112825b99775d282ed7ae3284c0
23489ac <23489ac@hvhs.school.nz>
2026-06-17 21:25:29 +0000

    Jun 17, 2026, 9:25 PM

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 console.log("Hello world!")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-function getFormInput() {
-let item = ["Cupcakes", 'Burgar",  "Fries", "Cheese Melt"];
-    const chocolate_Field = document.getElementById("chocolate_Field");
-    let userChoice = chocolate_Field.value;
-    OUTPUT.innerHTML += "<p>" + messages[userChoice] + "</p>"
-}
-function addItem() {
-    const itemField = document.getElementById("itemField");
-    let userItem = itemField.value;
-    shoppingList.push(userItem)
-    OUTPUT.innerHTML += "<p>You have added " + userItem + " to the list </p>";
 
-}
-function showList() {
-    OUTPUT.innerHTML += "<h3>These are the items on your shopping list:</h3>";
-    for (let i = 0; i < shoppingList.length; i++) {
-        OUTPUT.innerHTML += "<p>" + shoppingList[i] + "</p>";
-    }
+let menu = ["Cupcakes", "Burgar",  "Fries", "Cheese Melt"];
+let price = 5  
+function getFormInput() { 
+    const nameField = document.getElementById("nameField");
+    let name = nameField.value;
+
+    const itemField = document.getElementById("itemField");
+    let itemNumber = itemField.value;
+
+    const moneyField = document.getElementById("moneyField");
+    let money = moneyField.value;
 }
