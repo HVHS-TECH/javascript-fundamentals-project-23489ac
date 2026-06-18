eaff342a461f87bd24133fb2ab97a681ec0e460a
23489ac <23489ac@hvhs.school.nz>
2026-06-18 23:45:55 +0000

    Jun 18, 2026, 11:45 PM

--- script.js
+++ script.js
@@ -28,8 +28,7 @@ function getFormInput() {
         OUTPUT.innerHTML += "<p>Price: $" + food.price + "</p>";
         OUTPUT.innerHTML += "<p>Money Given: $" + userMoney + "</p>";
         OUTPUT.innerHTML += "<p>Change: $" + change + "</p>";
-        OUTPUT.innerHTML = "<h2>Menu</h2>";
-    
+        
     }
 }
 
