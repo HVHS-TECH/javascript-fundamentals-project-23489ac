31fe00d875d8a6c371ff93245e87cb86f6d3bcbf
23489ac <23489ac@hvhs.school.nz>
2026-06-19 00:17:11 +0000

    Jun 19, 2026, 12:17 AM

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 let menu = ["Cupcakes", "Burgar", "Fries", "Cheese Melt"];
 let price = ["4","8","5","6"];
 
+
+function calculateChange(_money, _price) {
+    return _money - _price;
+}
 function getFormInput() {
     const nameField = document.getElementById("nameField");
     const itemField = document.getElementById("itemField");
@@ -18,10 +22,8 @@ function getFormInput() {
     if (userMoney < itemprice) {
         OUTPUT.innerHTML = "<p>Sorry, you do not have enough money.</p>";
  } else {
-     let change = userMoney - item.price;
       var change = calculateChange(usermoney, PRICE)
-     function calculateChange(_money, _price) {
-    return _money - _price;
+
        OUTPUT.innerHTML = "";
         OUTPUT.innerHTML = "<h2>Receipt</h2>";
         OUTPUT.innerHTML += "<p>Name: " + userName + "</p>";
