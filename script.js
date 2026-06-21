d3b6f07b5c7f59a4bb8ee83e6a1993a46e2d2a53
23489ac <23489ac@hvhs.school.nz>
2026-06-21 23:44:28 +0000

    Jun 21, 2026, 11:44 PM

--- script.js
+++ script.js
@@ -4,25 +4,29 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 let menu = ["Cupcakes", "Burgar", "Fries", "Cheese Melt"];
 let price = ["4","8","5","6"];
 
-
-function calculateChange(_money, _price) {
-    return _money - _price;
-}
 function getFormInput() {
-    const nameField = document.getElementById("nameField");
-    const itemField = document.getElementById("itemField");
-    const moneyField = document.getElementById("moneyField");
+    OUTPUT.innerHTML = "";
 
-    let userName = nameField.value;
-    let useritem = itemField.value;
-    let userMoney = moneyField.value;
-    
-    let item = menu[userChoice - 1];
+    let name = document.getElementById("nameField").value;
+    let itemNumber = Number(document.getElementById("itemField").value);
+    let money = Number(document.getElementById("moneyField").value);
 
-    if (userMoney < itemprice) {
+    let food = menu[itemNumber - 1];
+    let foodPrice = Number(price[itemNumber - 1]);
+
+    if (money < foodPrice) {
         OUTPUT.innerHTML = "<p>Sorry, you do not have enough money.</p>";
- } else {
-      var change = calculateChange(usermoney, PRICE)
+    } else {
+        let change = money - foodPrice;
+
+        OUTPUT.innerHTML = "<h2>Receipt</h2>";
+        OUTPUT.innerHTML += "<p>Name: " + name + "</p>";
+        OUTPUT.innerHTML += "<p>Item: " + food + "</p>";
+        OUTPUT.innerHTML += "<p>Price: $" + foodPrice + "</p>";
+        OUTPUT.innerHTML += "<p>Money Given: $" + money + "</p>";
+        OUTPUT.innerHTML += "<p>Change: $" + change + "</p>";
+    }
+}
 
        OUTPUT.innerHTML = "";
         OUTPUT.innerHTML = "<h2>Receipt</h2>";
