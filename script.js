10909c40904a5b45aa5afcfe08a724109d4d72d2
23489ac <23489ac@hvhs.school.nz>
2026-06-29 00:39:51 +0000

    Jun 29, 2026, 12:39 AM

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ console.log("Hello world!")
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const MENU_OUTPUT = document.getElementById("menuDisplay");
 
+
+let menu = ["Cupcakes", "Burger", "Fries", "Cheese Melt"];
+let price = [4,8,5,6];
+
+
 let orderItem ="";
 let orderPrice =0;
 
@@ -14,13 +19,14 @@ MENU_OUTPUT.innerHTML = "<h2>Menu</h2>";
 }
 
 function addOrder(){
-const itemField = document.getElementById("itemField");
-let userItem =  itemField.value;  
+
+const ITEM_FIELD = document.getElementById("itemField");
+let itemNumber = ITEM_FIELD.value;  
 
 if (itemNumber>=1 && itemNumber <=4){
      orderItem = menu[itemNumber - 1];
-     orderMenu = price[itemnumber-1];
-     OUTPUT.innerHTML = "<p> You seleced" + orderItem +"</p>"
+     orderPrice = price[itemNumber-1];
+     OUTPUT.innerHTML = "<p> You selected " + orderItem + "</p>"
 
 }else{ 
       OUTPUT.innerHTML = "<P> Please choose a number between 1 to 4. </p>"
@@ -28,8 +34,6 @@ if (itemNumber>=1 && itemNumber <=4){
 }
 
 function placeOrder(){
-   let menu = ["Cupcakes", "Burgar", "Fries", "Cheese Melt"];
-   let price = ["4","8","5","6"];
    
     const NAME_FIELD = document.getElementById("nameField");
     const MONEY_FIELD = document.getElementById("moneyField");
@@ -38,15 +42,26 @@ function placeOrder(){
     let userMoney = MONEY_FIELD.value;
 
     if (userMoney >= orderPrice){
-         let change = (userMoney - price);
+         let change = userMoney - orderPrice ;
          OUTPUT.innerHTML ="<h2>  Receipt </h2>"
-         OUTPUT.innerHTML += "<p> Name :" + userName +"</p>"
-         OUTPUT.innerHTML += "<p> item:" + price +"</p>"
-         OUTPUT.innerHTML += "<p> Total cost:" + orderPrice +"</p>"
-         OUTPUT.innerHTML += "<p> The Money"+ userMoney+"</p>"
-         OUTPUT.innerHTML += "<p> The change" + change +"</p>"
+         OUTPUT.innerHTML += "<p> Name: " + userName + "</p>"
+         OUTPUT.innerHTML += "<p> item: " + orderItem + "</p>"
+         OUTPUT.innerHTML += "<p> Total cost: " + orderPrice + "</p>"
+         OUTPUT.innerHTML += "<p> Money Given "+ userMoney+ "</p>"
+         OUTPUT.innerHTML += "<p> The change: $" + change + "</p>"
 
     }else{
         OUTPUT.innerHTML = "<p> Sorry, you do not have enough money.</p>"
     }
+}
+function resetOrder() {
+
+    OUTPUT.innerHTML = "";
+    document.getElementById("nameField").value = "";
+    document.getElementById("itemField").value = "";
+    document.getElementById("moneyField").value = "";
+
+    orderItem = "";
+    orderPrice = 0;
+
 }
