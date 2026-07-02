cf6b2eeefc080cb4b4a33d26b9ccf5d779c5ab7b
23489ac <23489ac@hvhs.school.nz>
2026-07-02 23:38:30 +0000

    Jul 2, 2026, 11:38 PM

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function showMenu() {
 MENU_OUTPUT.innerHTML = "<h2>Menu</h2>";
  for (let i = 0; i < menu.length; i++) {
     MENU_OUTPUT.innerHTML += "<p>" + (i+1) + ". " + menu[i]  + " $"  + price[i]  + "</p>";
-
+     MENU_OUTPUT.innerHTML += "<img src='" + image[i] + "' width='100'><br><br>";
     }
 }
 
